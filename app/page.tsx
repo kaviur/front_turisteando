8285bcb9894fe6f95b831a3ba8aa78e95f1115ca
ravenbbs <julioc3bgud@gmail.com>
2024-10-28 21:06:38 -0500

    feat: initialize project structure

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "@/components/layout/Footer";
+import { Main } from "@/components/layout/Main";
+import { Navbar } from "@/components/layout/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
