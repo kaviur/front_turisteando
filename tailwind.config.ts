73085703bfd15458b42c255907f2bfbe3b22df27
Julio Condor <julioc3bgud@gmail.com>
2024-10-28 21:59:41 -0500

    feat: add colors configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,23 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ff0178",
+          secondary: "#ff5b03",
+          accent: "#bfdbfe",
+          neutral: "#f3f4f6",
+          "base-100": "#f3f4f6",
+          info: "#67e8f9",
+          success: "#34d399",
+          warning: "#fde047", 
+          error: "#f43f5e",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
 export default config;
