c87cbb19eb0e4fb8974a82ff6f83d058ced3b243
LiaTorres <lia.torres@dnest.io>
2024-10-30 20:54:25 -0300

    feat: add admin layout and header dashboard

---
+++ app/admin/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Header from "@/components/HeaderDashboard";
+import React, { useState } from "react";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <>
+      <div className="flex">
+        <div className="relative flex flex-1 flex-col lg:ml-72.5">
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
