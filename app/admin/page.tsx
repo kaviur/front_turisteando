7c3c0a20834f81841c21b6e5fe2acfe1cb96ffb4
Norlys Castañeda <castaneda30@hotmail.com>
2024-10-30 16:34:40 -0500

    chore: no significant changes to [page]

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,12 +2,14 @@ import { Footer } from "@/components/layout/Footer";
 import { Main } from "@/components/layout/Main";
 import { Navbar } from "@/components/layout/Navbar";
 
+
 export default function Home() {
   return (
     <>
       <Navbar />
       <Main />
       <Footer />
+      
     </>
   );
 }
