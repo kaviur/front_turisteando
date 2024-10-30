6100eb446de7a18b2454541f82a4240e3143063d
LiaTorres <lia.torres@dnest.io>
2024-10-30 20:57:42 -0300

    style: update admin pages styles

--- app/admin/addproduct/page.tsx
+++ app/admin/addproduct/page.tsx
@@ -1,11 +1,9 @@
 import { Footer } from "@/components/layout/Footer";
 import { Main } from "@/components/layout/Main";
-import { Navbar } from "@/components/layout/Navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Main />
       <Footer />
     </>
--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -13,11 +13,7 @@ export default function DashboardLayout({
       <div className="flex">
         <div className="relative flex flex-1 flex-col lg:ml-72.5">
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              {children}
-            </div>
-          </main>
+          <main>{children}</main>
         </div>
       </div>
     </>
