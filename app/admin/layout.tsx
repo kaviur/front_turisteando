aa71f7ec9f5ff39dd4389b5beabae6db49e47856
LiaTorres <lia.torres@dnest.io>
2024-10-31 17:59:15 -0300

    feat: add sidebar to admin layout and update styles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
-import Header from "@/components/HeaderAdmin";
+import HeaderAdmin from "@/components/HeaderAdmin";
+import Sidebar from "@/components/Sidebar";
 import React, { useState } from "react";
 
 export default function AdminLayout({
@@ -11,9 +12,17 @@ export default function AdminLayout({
   return (
     <>
       <div className="flex">
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <div className="relative flex flex-1 flex-col lg:ml-72.5">
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          <main>{children}</main>
+          <HeaderAdmin
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+          />
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
         </div>
       </div>
     </>
