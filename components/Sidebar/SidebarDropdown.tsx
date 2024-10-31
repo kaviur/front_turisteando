08aeb0d9e69a2673b21cc2488b10406081003280
LiaTorres <lia.torres@dnest.io>
2024-10-31 19:45:52 -0300

    refactor: update sidebarDropdown

--- components/Sidebar/SidebarDropdown.tsx
+++ components/Sidebar/SidebarDropdown.tsx
@@ -2,33 +2,26 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-interface SidebarDropdownItem {
-  label: string;
-  route: string;
-}
-
-interface SidebarDropdownProps {
-  item: SidebarDropdownItem[];
-}
-
-const SidebarDropdown: React.FC<SidebarDropdownProps> = ({ item }) => {
+const SidebarDropdown = ({ item }: any) => {
   const pathname = usePathname();
 
   return (
-    <ul className="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
-      {item.map((dropdownItem, index) => (
-        <li key={index}>
-          <Link
-            href={dropdownItem.route}
-            className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
-              pathname === dropdownItem.route ? "text-white" : ""
-            }`}
-          >
-            {dropdownItem.label}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
+        {item.map((item: any, index: number) => (
+          <li key={index}>
+            <Link
+              href={item.route}
+              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                pathname === item.route ? "text-white" : ""
+              }`}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
