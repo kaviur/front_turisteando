98b1ad4df45209e8923478a4423370ee990d86e6
LiaTorres <lia.torres@dnest.io>
2024-11-01 17:40:01 -0300

    style: update sidebar styles

--- components/Sidebar/SidebarItem.tsx
+++ components/Sidebar/SidebarItem.tsx
@@ -43,8 +43,8 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
         href={item.route}
         onClick={handleClick}
         className={`${
-          isItemActive ? "bg-graydark " : ""
-        } group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark`}
+          isItemActive ? "bg-secondary bg-opacity-10 text-secondary" : ""
+        } group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:text-secondary`}
       >
         {item.icon}
         {item.label}
