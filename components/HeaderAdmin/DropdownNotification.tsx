2410159bf9eca02e9fb2b6085aa08c038cb4281c
LiaTorres <lia.torres@dnest.io>
2024-11-01 17:41:58 -0300

    style: update header admin styles

--- components/HeaderAdmin/DropdownNotification.tsx
+++ components/HeaderAdmin/DropdownNotification.tsx
@@ -15,7 +15,7 @@ const DropdownNotification = () => {
             setDropdownOpen(!dropdownOpen);
           }}
           href="#"
-          className="relative flex h-8.5 w-8.5 items-center justify-center rounded-full border-[0.5px] border-stroke bg-gray hover:text-primary   "
+          className="relative flex h-8 w-8 items-center justify-center rounded-full border-[0.5px] border-stroke bg-gray hover:text-primary"
         >
           <span
             className={`absolute -top-0.5 right-0 z-1 h-2 w-2 rounded-full bg-meta-1 ${
@@ -42,9 +42,9 @@ const DropdownNotification = () => {
 
         {dropdownOpen && (
           <div
-            className={`absolute -right-27 mt-2.5 flex h-90 w-75 flex-col rounded-sm border border-stroke bg-white shadow-default   sm:right-0 sm:w-80`}
+            className={`absolute -right-27 mt-2 flex h-90 w-75 flex-col rounded-sm border border-stroke bg-white shadow-default   sm:right-0 sm:w-80`}
           >
-            <div className="px-4.5 py-3">
+            <div className="px-4 py-3">
               <h5 className="text-sm font-medium text-bodydark2">
                 Notification
               </h5>
@@ -53,7 +53,7 @@ const DropdownNotification = () => {
             <ul className="flex h-auto flex-col overflow-y-auto">
               <li>
                 <Link
-                  className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2"
+                  className="flex flex-col gap-2 border-t border-stroke px-4 py-3 hover:bg-gray-2"
                   href="#"
                 >
                   <p className="text-sm">
@@ -69,7 +69,7 @@ const DropdownNotification = () => {
               </li>
               <li>
                 <Link
-                  className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2"
+                  className="flex flex-col gap-2 border-t border-stroke px-4 py-3 hover:bg-gray-2"
                   href="#"
                 >
                   <p className="text-sm">
@@ -84,7 +84,7 @@ const DropdownNotification = () => {
               </li>
               <li>
                 <Link
-                  className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2"
+                  className="flex flex-col gap-2 border-t border-stroke px-4 py-3 hover:bg-gray-2"
                   href="#"
                 >
                   <p className="text-sm">
@@ -100,7 +100,7 @@ const DropdownNotification = () => {
               </li>
               <li>
                 <Link
-                  className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2"
+                  className="flex flex-col gap-2 border-t border-stroke px-4 py-3 hover:bg-gray-2"
                   href="#"
                 >
                   <p className="text-sm">
