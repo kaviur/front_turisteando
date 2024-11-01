e1b04e65f1137447500aeb6f007fc0f68317f06d
ravenbbs <julioc3bgud@gmail.com>
2024-11-01 17:34:25 -0500

    fix: admin not visible for mobile

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,34 @@
 "use client";
 import HeaderAdmin from "@/components/HeaderAdmin";
 import Sidebar from "@/components/Sidebar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isMobile, setIsMobile] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Definimos como "mobile" si el ancho es menor o igual a 768px
+    };
+    
+    handleResize(); // Comprobar en el primer render
+    window.addEventListener("resize", handleResize); // Agregar el evento al redimensionar la pantalla
+    return () => window.removeEventListener("resize", handleResize); // Limpiar el evento al desmontar
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className="flex items-center justify-center h-screen text-center p-4">
+        <p>Esta pantalla no esta disponible en dispositivos mobile, Por favor usa una computadora.</p>
+      </div>
+    );
+  }
+
   return (
     <html>
       <body>
