3894076134ce2a89e09715a1fa3c03d5e34ebfa9
LiaTorres <lia.torres@dnest.io>
2024-11-01 22:21:02 -0300

    style: update logo

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -147,7 +147,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
         }`}
       >
         {/* <!-- SIDEBAR HEADER --> */}
-        <div className="flex items-center justify-between gap-2 px-6 py-5 lg:py-6">
+        <div className="flex items-center justify-between gap-2 px-6 py-5 lg:py-6 ml-8">
           <Link href="/">
             <Image
               width={126}
