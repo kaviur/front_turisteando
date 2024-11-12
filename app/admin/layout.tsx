1078c4086a1b03d2ab4296678bb70fa96bdab69a
karen <quemojojojo@gmail.com>
2024-11-12 13:15:21 -0500

    feat(addproduct): create form

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -15,47 +15,35 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   const router = useRouter();
-  const [isMobile, setIsMobile] = useState(false);
-  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768); // Definimos como "mobile" si el ancho es menor o igual a 768px
-    };
-
-    handleResize(); // Comprobar en el primer render
-    window.addEventListener("resize", handleResize); // Agregar el evento al redimensionar la pantalla
-    return () => window.removeEventListener("resize", handleResize); // Limpiar el evento al desmontar
-  }, []);
-
-  if (isMobile) {
-    return (
-      <div className="flex flex-col items-center p-4 min-h-screen bg-base-100 gap-4 h-screen">
+  return (
+    <div>
+      {/* Vista móvil */}
+      <div className="flex flex-col items-center p-4 min-h-screen bg-base-100 gap-4 h-screen md:hidden">
         <div className="h-1/4">
-        <Image
-          width={600}
-          height={600}
-          src="/logo.png"
-          alt="Logo Turisteando"
-          className="w-full max-w-sm my-2 "
-        />
+          <Image
+            width={600}
+            height={600}
+            src="/logo.png"
+            alt="Logo Turisteando"
+            className="w-full max-w-sm my-2"
+          />
         </div>
-        
+
         <div className="flex flex-col gap-4 w-full">
-          <h2 className="text-4xl 2 font-bold mb-6">
+          <h2 className="text-4xl font-bold mb-6 flex items-center gap-2">
             <FaExclamationCircle />
-            Esta página no esta disponible para ver desde el móvil, por favor
-            ingresar desde una computadora{" "}
+            Esta página no está disponible para ver desde el móvil, por favor usa una computadora
           </h2>
 
-          <Link href={"/"} className="w-full">
+          <Link href="/" className="w-full">
             <PrimaryButton
               style="btn btn-lg btn-primary w-full rounded-full text-xl"
-              text="Ir a la pagina principal"
+              text="Ir a la página principal"
             />
           </Link>
 
-          <Link href={"/"} className="w-full ">
+          <Link href="/" className="w-full">
             <SecondaryButton
               onClick={() => router.back()}
               style="btn-lg btn-ghost border border-gray-200 border-2 text-gray-500 hover:text-gray-700 w-full rounded-full text-xl mb-2"
@@ -65,22 +53,18 @@ export default function AdminLayout({
           </Link>
         </div>
       </div>
-    );
-  }
 
-  return (
-    <div className="flex">
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      <div className="relative flex flex-1 flex-col lg:ml-72.5">
-        <HeaderAdmin
-          sidebarOpen={sidebarOpen}
-          setSidebarOpen={setSidebarOpen}
-        />
-        <main>
-          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-            {children}
-          </div>
-        </main>
+      {/* Vista para pantallas medianas o grandes */}
+      <div className="hidden md:flex">
+        <Sidebar sidebarOpen={true} setSidebarOpen={() => {}} />
+        <div className="relative flex flex-1 flex-col lg:ml-72.5">
+          <HeaderAdmin sidebarOpen={true} setSidebarOpen={() => {}} />
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   );
--- components/AddProductForm/AddProductForm.tsx
+++ components/AddProductForm/AddProductForm.tsx
@@ -1,6 +1,21 @@
+"use client";
+
 import PrimaryButton from "../ui/PrimaryButton";
+import { useState } from "react";
 
 const AddProductForm = () => {
+
+  // Estado para almacenar el nombre del archivo seleccionado
+  const [fileName, setFileName] = useState("Machu Picchu");
+
+  // Método que actualiza el nombre del archivo
+  const updateFileName = (event) => {
+    const input = event.target;
+    const name = input.files.length > 0 ? input.files[0].name : "Machu Picchu";
+    setFileName(name);
+  };  
+
+
   return (
     <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
       <div className="flex flex-col gap-9 w-[720px]">
@@ -13,7 +28,7 @@ const AddProductForm = () => {
           </div>
           <form action="#">
             <div className="p-6">
-              <div className="mb-4 flex flex-col gap-6 xl:flex-row">
+              <div className=" mb-6 flex flex-col gap-6 xl:flex-row">
                 <div className="w-full">
                   <label className="mb-3 block text-sm font-medium text-black">
                     Nombre
@@ -26,9 +41,36 @@ const AddProductForm = () => {
                 </div>
               </div>
 
-              <div className="mb-4">
+              <div className="mb-6">
+                <label className="mb-3 block text-sm font-medium text-black">
+                  Descripción
+                </label>
+                <textarea
+                  rows={6}
+                  placeholder="Ingresa la descripción del producto"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                ></textarea>
+              </div>
+
+              <div className="mb-6">
                 <label className="mb-3 block text-sm font-medium text-black">
                   Categoría
+                </label>
+                <select
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                  defaultValue=""
+                >
+                  <option value="" disabled>
+                    Selecciona la ciudad
+                  </option>
+                  <option value="lima">Lima</option>
+                  <option value="cusco">Cusco</option>
+                </select>
+              </div>
+
+              <div className="mb-6">
+                <label className="mb-3 block text-sm font-medium text-black">
+                  Ciudad
                 </label>
                 <select
                   className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
@@ -44,17 +86,6 @@ const AddProductForm = () => {
 
               <div className="mb-6">
                 <label className="mb-3 block text-sm font-medium text-black">
-                  Descripción
-                </label>
-                <textarea
-                  rows={6}
-                  placeholder="Ingresa la descripción del producto"
-                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
-                ></textarea>
-              </div>
-
-              <div className="mb-4">
-                <label className="mb-3 block text-sm font-medium text-black">
                   Precio
                 </label>
                 <input
@@ -63,6 +94,103 @@ const AddProductForm = () => {
                   className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
                 />
               </div>
+
+              <div className="grid grid-cols-2 gap-4 pt-4">
+                <div className="mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black">
+                  Disponibilidad Fecha de Inicio
+                  </label>
+                  <select
+                    className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                    defaultValue=""
+                  >
+                    <option value="" disabled>
+                      Seleccionar fecha
+                    </option>
+                    <option value="lima">12/11/2024</option>
+                    <option value="cusco">12/11/2024</option>
+                  </select>
+                </div>
+
+                <div className="mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black">
+                  Disponibilidad Fecha Fin
+                  </label>
+                  <select
+                    className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                    defaultValue=""
+                  >
+                    <option value="" disabled>
+                    Seleccionar fecha
+                    </option>
+                    <option value="lima">12/11/2024</option>
+                    <option value="cusco">12/11/2024</option>
+                  </select>
+                </div>
+
+                <div className="mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black">
+                    Capacidad
+                  </label>
+                  <input
+                    placeholder="Ingresar capacidad"
+                    className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                  ></input>
+                </div>
+
+                <div className="mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black">
+                  Duración
+                  </label>
+                  <select
+                    className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                    defaultValue=""
+                  >
+                    <option value="" disabled className="text-gray-400">
+                      Seleccionar duración
+                    </option>
+                    <option value="lima">12/11/2024</option>
+                    <option value="cusco">12/11/2024</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="mb-6">
+                <label className="mb-3 block text-sm font-medium text-black">
+                Características
+                </label>
+                <select
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                  defaultValue=""
+                  multiple
+                >
+                  <option value="" disabled className="text-gray-400">
+                    Selecciona las características
+                  </option>
+                  <option value="atributo1">Atributo 1</option>
+                  <option value="atributo2">Atributo 2</option>
+                  <option value="atributo3">Atributo 3</option>
+                  <option value="atributo4">Atributo 4</option>
+                </select>
+              </div>
+
+              <div class="mb-6">
+                <label class="block text-sm font-medium text-black mb-2">Imágenes</label>
+                <div class="flex items-center border border-gray-300 rounded overflow-hidden">
+                  <label class="bg-gray-100 text-gray-600 px-4 py-2 cursor-pointer hover:bg-gray-200">
+                    Selecciona el archivo
+                    <input type="file" class="hidden" onchange="updateFileName(this)" />
+                  </label>
+                  <input
+                    type="text"
+                    id="file-name"
+                    placeholder="Machu Picchu"
+                    class="flex-grow px-4 py-2 border-l border-gray-300 outline-none text-gray-700"
+                    readonly
+                  />
+                </div>
+              </div>
+
             </div>
           </form>
         </div>
