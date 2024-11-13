c16a3b927aa64de91742ba7cdeb3220041699fc1
karen <quemojojojo@gmail.com>
2024-11-13 08:08:46 -0500

    feat(dashboard): Reusable form for characteristics and categories

--- app/(protected)/admin/categories/page.tsx
+++ app/(protected)/admin/categories/page.tsx
@@ -1,11 +1,10 @@
-import AddProductForm from "@/components/AddProductForm/AddProductForm";
+import ReusableSmallForm from "@/components/ReusableSmallForm/ReusableSmallForm";
 
 export default function Home() {
   return (
     <>
       <div className="ml-96 flex justify-center">
-        {/* cambiar a componente categories */}
-        <AddProductForm />
+        <ReusableSmallForm entityType={"categoría"}/>
       </div>
     </>
   );
