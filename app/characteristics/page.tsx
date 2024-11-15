72eec54808ce72cc1db0f93d70ec1f520042fe26
ACarolinaGonzalezA <acarolina.gonzaleza@gmail.com>
2024-11-15 15:09:37 -0500

    feat/characteristics: add characteristics component

---
+++ app/characteristics/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { Footer } from "@/components/layout/Footer";
+import { Navbar } from "@/components/layout/Navbar";
+import { TabsPagination } from "@/components/TabsPagination";
+
+export default function Home() {
+  return (
+    <>
+      {" "}
+      <Navbar />
+      <div className="md:mt-28 mt-12  max-w-screen-2xl mx-auto ">
+        <TabsPagination isMobile={false} />
+      </div>
+      <Footer />
+    </>
+  );
+}
--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -218,7 +218,7 @@ const menuItems = [
           </svg>
         ),
         label: "Lista de Características",
-        route: "/admin/characteristic",
+        route: "/admin/characteristics",
       },
       {
         icon: (
@@ -241,7 +241,7 @@ const menuItems = [
           </svg>
         ),
         label: "Agregar Característica",
-        route: "/admin/addcharacteristics",
+        route: "/admin/addcharacteristic",
       },
     ],
   },
